278780987e32218e270394eb69bec5462b2314b3
James Jamail <james@jamesjamail.com>
2020-03-06 18:05:44 -0600

    amplify connected to appsync

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react";
+import React, { Component, Fragment } from "react";
+import { Header } from "semantic-ui-react";
+Amplify.configure(
+  aws_exports,
+  (Auth: {
+    // REQUIRED - Amazon Cognito Identity Pool ID
+    identityPoolId: "XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab",
+    // REQUIRED - Amazon Cognito Region
+    region: "XX-XXXX-X",
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: "XX-XXXX-X_abcd1234",
+    // OPTIONAL - Amazon Cognito Web Client ID
+    userPoolWebClientId: "XX-XXXX-X_abcd1234"
+  })
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header as="h1">Hello World!</Header>
+      </>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
